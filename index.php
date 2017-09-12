f180e7885b310f068f2624da3951b80d6176ead7
Daniel Kasai <danielkasai2012@gmail.com>
2017-09-12 22:02:03 +0200

    page with text, photo and random quote feature

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
 <?php
+require( "random_quote.php" );
+?>
 
-echo "Hello World";
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="CSCI E15 HES Course - Self Introduction">
+    <title>Self Introduction - Daniel Kasai</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
+    <link rel="stylesheet" href="custom_styles.css">
+</head>
+<body>
+
+<div class="container">
+    <h1>Daniel Kasai</h1>
+    <img src="photo_DK.jpg" alt="Photo of Daniel Kasai">
+
+    <h2>About Me</h2>
+    <p>Hi, my name is Daniel Kasai. I live in Hamburg, together with my wife Akiko Kasai.<br/>
+        After studying Philosophy, Media Science and Japan Studies, I work for a publishing company and as a freelance
+        illustrator.<br/>
+        I got more and more involved in programming and now aim for the Web Technologies Certificate, <br/>
+        to form a foundation for future web projects.<br/><br/>
+        I believe in the power of fresh air and the occasional walk :)
+    </p>
+    <h3>Random Quote</h3>
+    <blockquote><?= $random_quote ?></blockquote>
+</div>
+
+
+</body>
+</html>
